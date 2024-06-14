451da71619ee50d51c0f94a2093e1d97271810ea
msinkec <sinkec.mihael@gmail.com>
2024-06-14 17:38:37 +0200

    Cancel not needed when locked.

--- src/contracts/valutLocked.ts
+++ src/contracts/valutLocked.ts
@@ -15,33 +15,22 @@ export class VaultLocked extends SmartContract {
     withdrawalPubKey: PubKey
 
     @prop()
-    cancelPubKey: PubKey
-
-    @prop()
     vaultUnvaultedP2TROutput: ByteString
-
-    @prop()
-    vaultCancelP2TROutput: ByteString
 
     /**
      *
      * @param withdrawalPubKey - Public key, used for withdrawal.
      * @param cancelPubKey - Public key, used for canceling.
      * @param vaultUnvaultedP2TROutput - P2TR to the unvaulted state contract.
-     * @param vaultCancelP2TROutput - P2TR to the canceling state contract.
      *
      */
     constructor(
         withdrawalPubKey: PubKey,
-        cancelPubKey: PubKey,
-        vaultUnvaultedP2TROutput: ByteString,
-        vaultCancelP2TROutput: ByteString,
+        vaultUnvaultedP2TROutput: ByteString
     ) {
         super(...arguments)
         this.withdrawalPubKey = withdrawalPubKey
-        this.cancelPubKey = cancelPubKey
         this.vaultUnvaultedP2TROutput = vaultUnvaultedP2TROutput
-        this.vaultCancelP2TROutput = vaultCancelP2TROutput
     }
 
     @method()
@@ -60,26 +49,6 @@ export class VaultLocked extends SmartContract {
         // Check first output is P2TR to the unvaulted contract
         const hashOutputs = sha256(
             this.vaultUnvaultedP2TROutput + outputsSuffix
-        )
-        assert(hashOutputs == shPreimage.hashOutputs, 'hashOutputs mismatch')
-    }
-
-    @method()
-    public cancel(
-        shPreimage: SHPreimage,
-        sig: Sig,
-        outputsSuffix: ByteString
-    ) {
-        // Check sig.
-        assert(this.checkSig(sig, this.cancelPubKey))
-
-        // Check sighash preimage.
-        const s = SigHashUtils.checkSHPreimage(shPreimage)
-        assert(this.checkSig(s, SigHashUtils.Gx))
-
-        // Check first output is P2TR to the canceling contract
-        const hashOutputs = sha256(
-            this.vaultCancelP2TROutput + outputsSuffix
         )
         assert(hashOutputs == shPreimage.hashOutputs, 'hashOutputs mismatch')
     }
